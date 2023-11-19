cbfb5b2fff48d8a895969177c3e81995c9f8ff69
Rochman29 <rochmanbambanges29@upi.edu>
2023-11-19 20:54:29 +0700

    update function js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+document.getElementById("button1MoreInfo").style.display = "none";
 function cekKolomKosong() {
   var kolomInput = document.getElementById("email");
 
@@ -7,4 +8,39 @@ function cekKolomKosong() {
   }
 
   return true;
+}
+function resume() {
+  var x = document.getElementById("about");
+  var y = document.getElementById("work");
+  if (x.style.display && y.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "block";
+  } else {
+    x.style.display = "none";
+    y.style.display = "none";
+  }
+}
+function lessInfo1() {
+  var x = document.getElementById("info1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function lessInfo2() {
+  var x = document.getElementById("info2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function lessInfo3() {
+  var x = document.getElementById("info3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
